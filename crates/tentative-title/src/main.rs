de4d263f5afe1caa4faa0885b9481835766963ab
j1nxie <rylieeeeexd@gmail.com>
2024-04-25 21:09:01 +0700

    feat: generate a tile map

---
+++ crates/tentative-title/src/main.rs
@@ -0,0 +1,31 @@
+use bevy::{prelude::*, window::WindowResolution};
+
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use board_plugin::BoardPlugin;
+
+fn camera_setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn main() {
+    let mut app = App::new();
+
+    let mut primary_window = Window::default();
+    primary_window.resolution = WindowResolution::new(700.0, 800.0);
+    primary_window.title = "tentative-title 0.1.0-alpha".to_string();
+
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(primary_window),
+        exit_condition: bevy::window::ExitCondition::OnPrimaryClosed,
+        close_when_requested: true,
+    }));
+
+    app.add_plugins(BoardPlugin);
+
+    #[cfg(feature = "debug")]
+    app.add_plugins(WorldInspectorPlugin::new());
+
+    app.add_systems(Startup, camera_setup);
+    app.run();
+}
